0151c4f7cf3c8196088cbeefd3c21d0e6b747663
Priyank <Priyank.kumar@Xoriant.com>
2018-03-22 16:05:06 +0530

    First Commit of blue ocean

---
+++ BlueOcean/src/main/java/com/bo/ctrls/FileUploadController.java
@@ -0,0 +1,48 @@
+package com.bo.ctrls;
+
+import java.io.IOException;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.bo.constant.BOAppConstants;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+public class FileUploadController {
+	
+    @PostMapping("/upload/{file:.+}")
+    public @ResponseBody String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(BOAppConstants.UPLOADED_FOLDER +BOAppConstants.Separtor+ file.getOriginalFilename());
+            Files.write(path, bytes);
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    
+ 
+}
